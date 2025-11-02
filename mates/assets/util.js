6cd73f0627662fca7cf9a497c721485d7ac19825
Alex Morato <amorato@mail.com>
2025-11-02 23:50:41 +0100

    multiples changes

---
+++ mates/assets/util.js
@@ -0,0 +1,11 @@
+// util.js
+// Funciones utilitarias globales para los juegos
+
+// Mezcla un array (Fisher-Yates)
+export function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
